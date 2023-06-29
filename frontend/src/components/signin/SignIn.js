370b42a6b719c309c23e3e38d993cbb572f06d98
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-06-28 18:40:58 -0700

    signin rough draft

--- frontend/src/components/signin/SignIn.js
+++ frontend/src/components/signin/SignIn.js
@@ -1,23 +1,25 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function signin() {
+
+function SignIn() {
   return (
-    <div>
-        <div>
+    <div className='d-flex justify-content-center align-items-center bg-primary'>
+        <div className='p-3 bg-grey w-25'>
             <form action="">
-                <div>
+                <div className='mb-2'>
                     <label htmlFor='username'>Username</label>
-                    <input type="text" placeholder='Enter Username!'/>
+                    <input type="text" placeholder='Enter Username!' className='form-control'/>
                 </div>
-                <div>
+                <div className='mb-2'>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' placeholder='Enter Password!'/>
+                    <input type='password' placeholder='Enter Password!' className='form-control'/>
                 </div>
-                <button>Sign In</button>
+                <button className='btn btn-success'>Sign In</button>
             </form>
         </div>
     </div>
   )
 }
 
-export default signin
+export default SignIn
