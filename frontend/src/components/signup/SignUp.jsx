a82de2053cfdf9bdb764b465da9d81551df3ca32
arttony <50052508+arttony@users.noreply.github.com>
2023-06-29 08:00:07 -0700

    added a password field, sign up button, and button to return to login page

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -14,6 +14,10 @@ export const SignUp = () => {
         <form onSubmit={handleSubmit}>
             <label for="username">Username</label>
             <input type="text" placeholder='Enter a Username!' value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <label for="password">Password</label>
+            <input type="password" placeholder="Enter a Password" onChange={(e) => setPassword(e.target.value)}/>
+            <button>Sign Up</button>
+            <button>Already have an account login here!</button>
         </form>
 
         </>
