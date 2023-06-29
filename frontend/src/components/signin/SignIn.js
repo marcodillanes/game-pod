4d9deff1b3a6798abb499445c2d707b623f28ca0
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-06-28 19:10:14 -0700

    setting up for sql

--- frontend/src/components/signin/SignIn.js
+++ frontend/src/components/signin/SignIn.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function SignIn() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
   return (
+   
     <div className='d-flex vh-100 justify-content-center align-items-center bg-black'>
         <div className='p-3 bg-grey w-25'>
             <form action="">
                 <div className='mb-2'>
                     <label htmlFor='username'>Username</label>
-                    <input type="text" placeholder='Enter Username!' className='form-control'/>
+                    <input type="text" placeholder='Enter a Username!' className='form-control'
+                    onChange={e => setUsername(e.target.value)}/>
                 </div>
                 <div className='mb-2'>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' placeholder='Enter Password!' className='form-control'/>
+                    <input type='password' placeholder='Enter a Password!' className='form-control'
+                    onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <button className='btn btn-success'>Sign In</button>
             </form>
