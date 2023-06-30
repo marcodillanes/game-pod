e77adec28896ba45e62caf53bb9390d6c80d3cbb
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-06-29 20:02:30 -0700

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import SignIn from "./components/signin/SignIn.js";
 import { SignUp } from "./components/signup/SignUp.jsx";
-import { BrowserRouter, Routers, Router } from 'react-router-dom';
+import { BrowserRouter, Routers, Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div>
+ <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<SignIn/>}></Route>
+    <Route path="/SignUp" element={<SignUp/>}></Route>
+  </Routes>
  
-      <SignIn />
-
-      <SignUp/>
-
-    </div>
-  );
+ 
+ </BrowserRouter>
 }
 
 export default App;
+
+// {/* <div>
+ 
+// <SignIn />
+
+// <SignUp/>
+
+// </div> */}
