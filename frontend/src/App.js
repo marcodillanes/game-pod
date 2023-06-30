d2c7ff248921bc7f8d0cb78b3c22d5fc2301831d
arttony <50052508+arttony@users.noreply.github.com>
2023-06-29 18:32:21 -0700

    added a black bg color and centered the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SignIn from "./components/signin/SignIn.js";
-import { SignUp } from "./components/signup/SignUp.jsx"
+import { SignUp }   from "./components/signup/SignUp.jsx"
 
 function App() {
   return (
--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -11,19 +11,21 @@ export const SignUp = () => {
     return (
         <>Sign Up  
         
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label for="username">Username</label>
-                <input type="text" placeholder='Enter a Username!' value={username} onChange={(e) => setUsername(e.target.value)} required/>
-            </div>
-            <div>
-                <label for="password">Password Must contain at least one number and one uppercase and one lowercase letter, and at least 6 or more characters</label>
-                <input type="password" placeholder="Enter a Password" value={password} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" onChange={(e) => setPassword(e.target.value)} required/>
-            </div>
+        <div className='d-flex vh-100 justify-content-center align-items-center bg-black'>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" placeholder='Enter a Username!' value={username} onChange={(e) => setUsername(e.target.value)} required/>
+                </div>
+                <div>
+                    <label for="password">Password Must contain at least one number and one uppercase and one lowercase letter, and at least 6 or more characters</label>
+                    <input type="password" placeholder="Enter a Password" value={password} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" onChange={(e) => setPassword(e.target.value)} required/>
+                </div>
             
-            <button>Sign Up</button>
-            
+            <button>Sign Up</button>  
         </form>
+        </div>
+        
         
         <button>Already have an account? Login here!</button>
         </>
