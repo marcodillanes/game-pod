ce5284ec00b784f56330f97e39328c895c20542b
arttony <50052508+arttony@users.noreply.github.com>
2023-07-02 13:07:11 -0700

    created post route for sign up

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require ('cors');
+const dotenv = require("dotenv").config()
  
 const app = express();
 
@@ -10,6 +11,13 @@ const db = mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
+})
+
+app.post('/signup', (req, res)=> {
+    db.query("INSERT INTO user_info (username, password) VALUES (?,?)", 
+    [username, password], 
+    (err, result) => console.log(err)
+    )
 })
 
 app.listen(5000, () => {
--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom'
 
 export const SignUp = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [usernameReg, setUsernameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
     const navigate = useNavigate()
 
     function handleSubmit(event) {
@@ -12,20 +12,20 @@ export const SignUp = () => {
     }
 
     return (
-        <>Sign Up  
+        <>  
         
         <div className='d-flex vh-100 justify-content-center align-items-center bg-black'>
             <div className='p-3 bg-grey w-25'>
                 <form onSubmit={handleSubmit}>
                     <div className='mb-2'>
                         <label for="username">Username</label>
-                        <input type="text" placeholder='Enter a Username!' value={username} className="form-control" 
-                        onChange={(e) => setUsername(e.target.value)} required/>
+                        <input type="text" placeholder='Enter a Username!' value={usernameReg} className="form-control" 
+                        onChange={(e) => setUsernameReg(e.target.value)} required/>
                     </div>
                     <div className='mb-2'>
                         <label for="password">Password </label>
-                        <input type="password" placeholder="At least one number, upper, and lowercase letter" value={password} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" className="form-control"
-                        onChange={(e) => setPassword(e.target.value)} required/>
+                        <input type="password" placeholder="At least one number, upper, and lowercase letter" value={passwordReg} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" className="form-control"
+                        onChange={(e) => setPasswordReg(e.target.value)} required/>
                     </div>
             
                 <button className='btn btn-success mb-2'>Sign Up</button>  
